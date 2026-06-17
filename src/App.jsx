78589510575c5736ab3c59a21dda460dad09f8f9
HanyaSeorangFerdi <ferdian.pratama060606@gmail.com>
2026-06-17 22:25:27 +0700

    Add page routing and Navbar to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
+import { useState } from "react";
+import Home from "./pages/Home";
+import RiwayatPage from "./pages/RiwayatPage";
+import TambahPage from "./pages/TambahPage";
+import LaporanPage from "./pages/LaporanPage";
+import Navbar from "./components/Navbar";
 
+export default function App() {
+  const [page, setPage] = useState("home");
+
+  return (
+    <>
+      {page === "home" && <Home />}
+      {page === "riwayat" && <RiwayatPage />}
+      {page === "tambah" && <TambahPage />}
+      {page === "laporan" && <LaporanPage />}
+
+      <Navbar page={page} setPage={setPage} />
+    </>
+  );
+}
