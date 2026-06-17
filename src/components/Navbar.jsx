a3a479e4db718ec299287b24016d132be6deb0b4
HanyaSeorangFerdi <ferdian.pratama060606@gmail.com>
2026-06-17 22:27:53 +0700

    Add Navbar component with navigation buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,10 @@
-
+export default function Navbar({ page, setPage }) {
+  return (
+    <div>
+      <button onClick={() => setPage("home")}>Home</button>
+      <button onClick={() => setPage("riwayat")}>Riwayat</button>
+      <button onClick={() => setPage("tambah")}>Tambah</button>
+      <button onClick={() => setPage("laporan")}>Laporan</button>
+    </div>
+  );
+}
