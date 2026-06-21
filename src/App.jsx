4b02505c098aba3f88ded5a5e46e743a418550e2
HanyaSeorangFerdi <ferdian.pratama060606@gmail.com>
2026-06-21 13:31:09 +0700

    Refactor App component layout and structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
+
 import Home from "./pages/Home";
 import RiwayatPage from "./pages/RiwayatPage";
 import TambahPage from "./pages/TambahPage";
 import LaporanPage from "./pages/LaporanPage";
+
 import Navbar from "./components/Navbar";
 
 export default function App() {
@@ -10,13 +12,19 @@ export default function App() {
   const [page, setPage] = useState("home");
 
   return (
-    <>
+    <div>
+
       {page === "home" && <Home />}
       {page === "riwayat" && <RiwayatPage />}
       {page === "tambah" && <TambahPage />}
       {page === "laporan" && <LaporanPage />}
 
-      <Navbar page={page} setPage={setPage} />
-    </>
+      <Navbar
+        page={page}
+        setPage={setPage}
+      />
+
+    </div>
   );
+
 }
