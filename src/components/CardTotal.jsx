ad4f19cac2aaa1f6f0a5195abcbf38a079d04ec2
HanyaSeorangFerdi <ferdian.pratama060606@gmail.com>
2026-06-21 13:35:14 +0700

    Add color prop and style to CardTotal component

--- src/components/CardTotal.jsx
+++ src/components/CardTotal.jsx
@@ -1,10 +1,33 @@
-export default function CardTotal({ title, amount }) {
+export default function CardTotal({
+  title,
+  amount,
+  color
+}) {
 
   return (
-    <>
-      <h3>{title}</h3>
-      <h1>{amount}</h1>
-    </>
+    <div
+      style={{
+        background: color,
+        borderRadius: 20,
+        padding: 20,
+        color: "#fff",
+        marginBottom: 15
+      }}
+    >
+      <div style={{ fontSize: 14 }}>
+        {title}
+      </div>
+
+      <div
+        style={{
+          fontSize: 28,
+          fontWeight: "bold",
+          marginTop: 10
+        }}
+      >
+        Rp {amount.toLocaleString("id-ID")}
+      </div>
+    </div>
   );
 
 }
