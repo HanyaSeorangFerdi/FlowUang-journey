ff5c95bf92d259ce825023490e6dc0a9481f5a44
HanyaSeorangFerdi <ferdian.pratama060606@gmail.com>
2026-06-21 13:43:43 +0700

    Update API_URL and implement saveData function

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,20 @@
-const API_URL = "";
+const API_URL = "1C4o5N_poO6E-g6xoBJ0r4FCwXnf9p02I9Rt5F-Uhev8";
 
 export async function getData() {
+
+  const response = await fetch(API_URL);
+
+  const result = await response.json();
+
+  return result.data;
 
 }
 
 export async function saveData(data) {
+
+  await fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify(data)
+  });
 
 }
