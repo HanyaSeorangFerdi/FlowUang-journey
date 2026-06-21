76c46e4f4519cc88777d776b0d3f15a1b1c8574c
HanyaSeorangFerdi <ferdian.pratama060606@gmail.com>
2026-06-21 13:35:54 +0700

    Update CategoryChart.jsx

--- src/components/CategoryChart.jsx
+++ src/components/CategoryChart.jsx
@@ -1,9 +1,22 @@
 export default function CategoryChart() {
 
   return (
-    <>
-      Category Chart
-    </>
+    <div
+      style={{
+        background: "#fff",
+        borderRadius: 20,
+        padding: 20
+      }}
+    >
+      <h3>Kategori Pengeluaran</h3>
+
+      <ul>
+        <li>Makanan</li>
+        <li>Transportasi</li>
+        <li>Belanja</li>
+        <li>Lainnya</li>
+      </ul>
+    </div>
   );
 
 }
